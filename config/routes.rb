088127d62f480ee15a36ead75923c1a1336416c1
Max Carlquist <tenkakle@gmail.com>
2018-05-29 13:12:17 +0200

    sign in users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get '/about', to: 'pages#about'
-  
+  get 'signup', to: 'users#new'
   resources :articles
+
+  #post 'users' to: 'users#new' one way of posting
+  resources :users, except: [:new]
 end
